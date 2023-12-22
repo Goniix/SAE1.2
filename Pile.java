4520350e47e4d668ba05171fc5d04b79efc92dc2
Gabriel Redouin-Innecco <dragonixgold@gmail.com>
2023-12-22 14:14:17 +0100

    started dynamic Pile class

---
+++ Pile.java
@@ -0,0 +1,32 @@
+class Pile{
+    int[] c = new int[0];
+
+    public void rebuild(int size){
+        int[] newPile = new int[cardCount];
+        int refIndex = 0;
+        int oldIndex = 0;
+        while(refIndex<cardCount && oldIndex<length(pile)){
+            if(pile[oldIndex] != -1){
+                newPile[refIndex] = pile[oldIndex];
+                refIndex++;
+            }
+            oldIndex++;
+        }
+        this.c = newPile;
+    }
+
+    public void append(int elem){
+        this.rebuild(this.length()+1);
+        this.c[this.length()-1] = elem;
+    }
+
+    public int length(){
+        return length(this.c);
+    }
+
+    public int pop(){
+        int res = this.c[this.length()-1];
+        this.rebuild(this.length()-1);
+        return res;
+    }
+}
