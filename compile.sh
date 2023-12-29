c5809372f7c22e0f8ef48e989c6cfaa31ca286de
Goniix <dragonixgold@gmail.com>
2023-12-29 22:55:40 +0100

    rebashed sh files

--- compile.sh
+++ compile.sh
@@ -4,3 +4,5 @@ export CLASSES=classes
 export CLASSPATH=`find lib -name "*.jar" | tr '\n' ':'`
 
 javac -cp ${CLASSPATH} -sourcepath ${SOURCES} -d ${CLASSES} $@ `find src -name "*.java"`
+#si ça marche pas faut run
+#sed -i -e 's/\r$//' compile.sh
--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ export CLASSPATH=`find ./lib -name "*.jar" | tr '\n' ':'`
 export MAINCLASS='Splask' # <- à remplacer par le nom de votre programme
 java -cp ${CLASSPATH}:classes $MAINCLASS
 # Le programme s'exécute depuis la racine de l'archive
+
+#si ça marche pas faut run
+#sed -i -e 's/\r$//' run.sh
